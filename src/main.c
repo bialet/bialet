c8492030bdeb8c8a426f1aacf1728cdd0bacf7d1
Rodrigo Arce <rsarce@gmail.com>
2023-09-15 13:22:09 -0300

    Add colors messages

--- src/main.c
+++ src/main.c
@@ -16,12 +16,58 @@ struct BialetResponse {
   char *body;
 };
 
+/* Configs */
+int color = 1;
+int output = 1;
+int debug = 0;
+
+/* Wren */
 char *wrenBuffer = 0;
 WrenConfiguration wrenConfig;
 
-/*
- ** Allocate memory safely
- */
+char *colorize(char *str, int color) {
+  if (!color) {
+    return str;
+  }
+  char *output = malloc(100);
+  sprintf(output, "\033[%dm%s\033[0m", color, str);
+  return output;
+}
+
+char *green(char *str) { return colorize(str, 32); }
+char *red(char *str) { return colorize(str, 31); }
+char *blue(char *str) { return colorize(str, 34); }
+char *yellow(char *str) { return colorize(str, 33); }
+
+void message_internal(int num, ...) {
+  va_list args;
+  va_start(args, num);
+
+  for (int i = 0; i < num; ++i) {
+    char *str = va_arg(args, char *);
+    printf("%s", str);
+    if (i < num - 1) {
+      printf(" ");
+    }
+  }
+
+  printf("\n");
+  va_end(args);
+}
+
+#define message_1(x) message_internal(1, x)
+#define message_2(x, y) message_internal(2, x, y)
+#define message_3(x, y, z) message_internal(3, x, y, z)
+#define message_4(w, x, y, z) message_internal(4, w, x, y, z)
+#define message_5(v, w, x, y, z) message_internal(5, v, w, x, y, z)
+#define message_6(u, v, w, x, y, z) message_internal(6, u, v, w, x, y, z)
+#define message_7(t, u, v, w, x, y, z) message_internal(7, t, u, v, w, x, y, z)
+#define message_8(s, t, u, v, w, x, y, z) message_internal(8, s, t, u, v, w, x, y, z)
+#define message_9(r, s, t, u, v, w, x, y, z) message_internal(9, r, s, t, u, v, w, x, y, z)
+
+#define GET_MACRO(_1, _2, _3, _4, _5, _6, _7, _8, _9, NAME, ...) NAME
+#define message(...) GET_MACRO(__VA_ARGS__, message_9, message_8, message_7, message_6, message_5, message_4, message_3, message_2, message_1)(__VA_ARGS__)
+
 static char *SafeMalloc(size_t size) {
   char *p;
 
@@ -161,7 +207,7 @@ static void httpHandler(struct mg_connection *c, int ev, void *ev_data,
   }
 }
 
-static void initConfig() { printf("Reload config\n"); }
+static void initConfig() { message(green("Reload config")); }
 
 static void *fileWatcher(void *arg) {
   int length, i = 0;
@@ -199,8 +245,7 @@ static void *fileWatcher(void *arg) {
 }
 
 void welcome() {
-  printf("🚲 \033[32mbialet\033[0m is riding on \033[34m%s://%s:%d\033[0m\n",
-         "http", "localhost", 8080);
+  message("🚲", green("bialet"), "is riding on", blue("http://localhost:8080"));
 }
 
 int main() {
