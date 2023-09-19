2ea7ef12bc57d4c7450c2338b9dc932d9ed19235
Rodrigo Arce <rsarce@gmail.com>
2023-09-19 03:29:00 -0300

    Memory and CPU limits

--- src/main.c
+++ src/main.c
@@ -8,6 +8,10 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
 #include <sys/inotify.h>
 
 #define EVENT_SIZE (sizeof(struct inotify_event))
@@ -75,7 +79,16 @@ void welcome() {
   message("🚲", green("bialet"), "is riding on", blue(serverUrl()));
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    pid_t pid;
+    int status;
+    struct rlimit mem_limit;
+    mem_limit.rlim_cur = 50 * 1024 * 1024;  // 50 MB soft limit
+    mem_limit.rlim_max = 100 * 1024 * 1024;  // 100 MB hard limit
+    struct rlimit cpu_limit;
+    cpu_limit.rlim_cur = 15;
+    cpu_limit.rlim_max = 30;
+
 
   struct mg_mgr mgr;
 
@@ -91,7 +104,28 @@ int main() {
   pthread_create(&thread_id, NULL, fileWatcher, NULL);
 
   for (;;) {
+      pid = fork();
+        if (pid == 0) {
+            // Set cpu time and memory limit
+            if (setrlimit(RLIMIT_AS, &mem_limit) == -1 || setrlimit(RLIMIT_CPU, &cpu_limit) == -1) {
+                perror("setrlimit");
+                exit(1);
+            }
+  for (;;) {
     mg_mgr_poll(&mgr, 1000);
+  }
+        } else if (pid > 0) {
+            // Parent: Wait for child to exit
+            wait(&status);
+            if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+                break;
+            } else {
+                message(red("Error"), "Restarting");
+            }
+        } else {
+            perror("fork");
+            exit(1);
+        }
   }
 
   return 0;
