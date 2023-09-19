a1f0f2ba72c38b2f034c776a0632a16e17eb6ecb
Rodrigo Arce <rsarce@gmail.com>
2023-09-19 03:33:01 -0300

    Coding styling

--- src/main.c
+++ src/main.c
@@ -8,15 +8,16 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <sys/inotify.h>
+#include <sys/resource.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/resource.h>
-#include <sys/inotify.h>
+#include <unistd.h>
 
 #define EVENT_SIZE (sizeof(struct inotify_event))
 #define BUF_LEN (1024 * (EVENT_SIZE + 16))
 #define MAX_URL_LEN 200
+#define MEGABYTE (1024 * 1024)
 
 /* Configs */
 int port = 8080;
@@ -80,15 +81,14 @@ void welcome() {
 }
 
 int main(int argc, char *argv[]) {
-    pid_t pid;
-    int status;
-    struct rlimit mem_limit;
-    mem_limit.rlim_cur = 50 * 1024 * 1024;  // 50 MB soft limit
-    mem_limit.rlim_max = 100 * 1024 * 1024;  // 100 MB hard limit
-    struct rlimit cpu_limit;
-    cpu_limit.rlim_cur = 15;
-    cpu_limit.rlim_max = 30;
-
+  pid_t pid;
+  int status;
+  struct rlimit mem_limit;
+  mem_limit.rlim_cur = 50 * MEGABYTE;  // 50 MB soft limit
+  mem_limit.rlim_max = 100 * MEGABYTE; // 100 MB hard limit
+  struct rlimit cpu_limit;
+  cpu_limit.rlim_cur = 15;
+  cpu_limit.rlim_max = 30;
 
   struct mg_mgr mgr;
 
@@ -104,28 +104,29 @@ int main(int argc, char *argv[]) {
   pthread_create(&thread_id, NULL, fileWatcher, NULL);
 
   for (;;) {
-      pid = fork();
-        if (pid == 0) {
-            // Set cpu time and memory limit
-            if (setrlimit(RLIMIT_AS, &mem_limit) == -1 || setrlimit(RLIMIT_CPU, &cpu_limit) == -1) {
-                perror("setrlimit");
-                exit(1);
-            }
-  for (;;) {
-    mg_mgr_poll(&mgr, 1000);
-  }
-        } else if (pid > 0) {
-            // Parent: Wait for child to exit
-            wait(&status);
-            if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
-                break;
-            } else {
-                message(red("Error"), "Restarting");
-            }
-        } else {
-            perror("fork");
-            exit(1);
-        }
+    pid = fork();
+    if (pid == 0) {
+      // Set cpu time and memory limit
+      if (setrlimit(RLIMIT_AS, &mem_limit) == -1 ||
+          setrlimit(RLIMIT_CPU, &cpu_limit) == -1) {
+        perror("setrlimit");
+        exit(1);
+      }
+      for (;;) {
+        mg_mgr_poll(&mgr, 1000);
+      }
+    } else if (pid > 0) {
+      // Parent: Wait for child to exit
+      wait(&status);
+      if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+        break;
+      } else {
+        message(red("Error"), "Restarting");
+      }
+    } else {
+      perror("fork");
+      exit(1);
+    }
   }
 
   return 0;
