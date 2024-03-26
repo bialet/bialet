5f5c65819a3272dd88b73871280028728bb7ec50
Rodrigo Arce <albo@pragmore.com>
2024-03-26 17:12:53 -0300

    Handling root path and port in GUI

--- gui/preload.js
+++ gui/preload.js
@@ -1,7 +1,8 @@
 const { contextBridge, ipcRenderer, shell } = require('electron')
 
 contextBridge.exposeInMainWorld('bialet', {
-  start: (port, path, logCallback, statusChanged) => ipcRenderer.send('start', port, path, logCallback, statusChanged),
-  stop: (bialet) => ipcRenderer.send('stop', bialet),
+  start: (port, path) => ipcRenderer.invoke('start', port, path),
+  stop: () => ipcRenderer.invoke('stop'),
   openExternal: (href) => ipcRenderer.invoke('openExternal', href),
+  selectFolder: () => ipcRenderer.invoke('dialog:openDirectory'),
 })
