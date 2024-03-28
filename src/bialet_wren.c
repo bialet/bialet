213c36191f43263a2dc053d7cf4250dccac982f5
Rodrigo Arce <albo@pragmore.com>
2024-03-27 21:51:33 -0300

    Add Windows implementation of http call and refactor utils functions

--- src/bialet_wren.c
+++ src/bialet_wren.c
@@ -15,6 +15,7 @@
 #include "http_call.h"
 #include "messages.h"
 #include "mongoose.h"
+#include "utils.h"
 #include "wren.h"
 #include "wren_vm.h"
 #include <openssl/rand.h>
@@ -36,49 +37,6 @@
 WrenConfiguration wren_config;
 static struct BialetConfig bialet_config;
 sqlite3 *db;
-
-static char *safe_malloc(size_t size) {
-  char *p;
-
-  p = (char *)malloc(size);
-  if (p == 0) {
-    exit(1);
-  }
-  return p;
-}
-
-static char *string_safe_copy(const char *zSrc) {
-  char *zDest;
-  size_t size;
-
-  if (zSrc == 0)
-    return 0;
-  size = strlen(zSrc) + 1;
-  zDest = (char *)safe_malloc(size);
-  strcpy(zDest, zSrc);
-  return zDest;
-}
-
-static char *string_append(char *zPrior, const char *zSep, const char *zSrc) {
-  char *zDest;
-  size_t size;
-  size_t n0, n1, n2;
-
-  if (zSrc == 0)
-    return 0;
-  if (zPrior == 0)
-    return string_safe_copy(zSrc);
-  n0 = strlen(zPrior);
-  n1 = strlen(zSep);
-  n2 = strlen(zSrc);
-  size = n0 + n1 + n2 + 1;
-  zDest = (char *)safe_malloc(size);
-  memcpy(zDest, zPrior, n0);
-  free(zPrior);
-  memcpy(&zDest[n0], zSep, n1);
-  memcpy(&zDest[n0 + n1], zSrc, n2 + 1);
-  return zDest;
-}
 
 static void wren_write(WrenVM *vm, const char *text) {
   message(yellow("Log"), text);
---
+++ src/utils.c
@@ -0,0 +1,48 @@
+#include "utils.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *safe_malloc(size_t size) {
+  char *p;
+
+  p = (char *)malloc(size);
+  if (p == 0) {
+    exit(1);
+  }
+  return p;
+}
+
+char *string_safe_copy(const char *zSrc) {
+  char *zDest;
+  size_t size;
+
+  if (zSrc == 0)
+    return 0;
+  size = strlen(zSrc) + 1;
+  zDest = (char *)safe_malloc(size);
+  strcpy(zDest, zSrc);
+  return zDest;
+}
+
+char *string_append(char *zPrior, const char *zSep, const char *zSrc) {
+  char *zDest;
+  size_t size;
+  size_t n0, n1, n2;
+
+  if (zSrc == 0)
+    return 0;
+  if (zPrior == 0)
+    return string_safe_copy(zSrc);
+  n0 = strlen(zPrior);
+  n1 = strlen(zSep);
+  n2 = strlen(zSrc);
+  size = n0 + n1 + n2 + 1;
+  zDest = (char *)safe_malloc(size);
+  memcpy(zDest, zPrior, n0);
+  free(zPrior);
+  memcpy(&zDest[n0], zSep, n1);
+  memcpy(&zDest[n0 + n1], zSrc, n2 + 1);
+  return zDest;
+}
+
