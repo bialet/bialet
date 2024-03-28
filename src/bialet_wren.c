2ea44307fb3a53882fa49932e05fe5bbfdaa8cef
Rodrigo Arce <albo@pragmore.com>
2024-03-27 21:31:43 -0300

    Remove versioning in OpenSSL

--- src/bialet_wren.c
+++ src/bialet_wren.c
@@ -280,7 +280,6 @@ static void random_string(WrenVM *vm) {
 }
 
 static void hash_password(WrenVM *vm) {
-#if OPENSSL_VERSION_NUMBER > 0x102031af
   const char *password = wrenGetSlotString(vm, 1);
   unsigned char salt[16];
   if (!RAND_bytes(salt, sizeof(salt))) {
@@ -312,11 +311,9 @@ static void hash_password(WrenVM *vm) {
 
   wrenEnsureSlots(vm, 2);
   wrenSetSlotString(vm, 0, result);
-#endif
 }
 
 static void verify_password(WrenVM *vm) {
-#if OPENSSL_VERSION_NUMBER > 0x102031af
   int result = 0;
   const char *password = wrenGetSlotString(vm, 1);
   const char *hash_and_salt = wrenGetSlotString(vm, 2);
@@ -351,7 +348,6 @@ static void verify_password(WrenVM *vm) {
   result = strcmp(new_hash_str, stored_hash) == 0;
   wrenEnsureSlots(vm, 2);
   wrenSetSlotBool(vm, 0, result);
-#endif
 }
 
 static void http_call(WrenVM *vm) {
