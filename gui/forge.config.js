5ef2aa3edb259a12a4f07f683afb8e856b4a37b9
Rodrigo Arce <albo@pragmore.com>
2024-04-01 22:43:30 -0300

    Add build tool

--- gui/forge.config.js
+++ gui/forge.config.js
@@ -1,35 +1,23 @@
 const FusesPlugin = require('@electron-forge/plugin-fuses').FusesPlugin;
 const FuseV1Options = require('@electron/fuses').FuseV1Options;
 const FuseVersion = require('@electron/fuses').FuseVersion;
-const os = require('os');
-let osPath
-if (os.platform() === 'win32') {
-  osPath = 'win'
-} else if (os.platform() === 'darwin') {
-  osPath = 'mac'
-} else {
-  osPath = 'linux'
-}
 
 exports.packagerConfig = {
   asar: true,
-  extraFiles: [{
-    "from": `bin/${osPath}`,
-    "to": "bin",
-    "filter": [
-      "**/*"
-    ]
-  }],
+  ignore: [
+    '**/node_modules/**',
+    '**/out/**',
+    '**/bin/**',
+    '**/src/**',
+    '**/docs/**'
+  ],
   'icon': './gui/icon',
 };
   exports.rebuildConfig = {};
   exports.makers = [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {
-        'author': 'Rodrigo Arce',
-        'description': 'Bialet Desktop is a graphical user interface for Bialet.'
-      },
+      config: {},
     },
     {
       name: '@electron-forge/maker-zip',
@@ -40,8 +28,6 @@ exports.packagerConfig = {
       config: {
         options: {
           icon: './gui/icon.png',
-          mantainer: 'Rodrigo Arce',
-          homepage: 'https://bialet.dev'
         },
       },
     }
