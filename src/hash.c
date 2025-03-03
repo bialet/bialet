365b8a602284baee03c936f4c9fd69336c26575c
Rodrigo Arce <albo@pragmore.com>
2025-03-02 21:11:43 -0300

    Add unsafe password and verify without using OpenSSL

--- src/hash.c
+++ src/hash.c
@@ -2,9 +2,32 @@
 
 #include <string.h>
 
+#ifndef OPENSSL_OK
+#define SALT_LENGTH 16
+#define HASH_LENGTH 64
+
+void unsafe_hash(const char* input, char* output) {
+  unsigned int hash = 5381;
+  int          c;
+  while((c = *input++))
+    hash = ((hash << 5) + hash) + c; // hash * 33 + c
+  snprintf(output, HASH_LENGTH + 1, "%08x%08x%08x%08x", hash, hash * 3, hash * 5,
+           hash * 7);
+}
+
+void generate_salt(char* salt, size_t length) {
+  for(size_t i = 0; i < length; i++) {
+    salt[i] =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"[rand() %
+                                                                         62];
+  }
+  salt[length] = '\0';
+}
+#endif
+
 void hashPassword(WrenVM* vm) {
+  const char* password = wrenGetSlotString(vm, 1);
 #ifdef OPENSSL_OK
-  const char*   password = wrenGetSlotString(vm, 1);
   unsigned char salt[16];
   if(!RAND_bytes(salt, sizeof(salt))) {
     perror("Failed to generate salt");
@@ -33,7 +56,14 @@ void hashPassword(WrenVM* vm) {
     sprintf(result + strlen(result), "%02x", salt[i]);
   }
 #else
-  char* result = "";
+  char salt[SALT_LENGTH + 1];
+  generate_salt(salt, SALT_LENGTH);
+  char saltedPassword[strlen(password) + SALT_LENGTH + 1];
+  snprintf(saltedPassword, sizeof(saltedPassword), "%s%s", password, salt);
+  char hash[HASH_LENGTH + 1];
+  unsafe_hash(saltedPassword, hash);
+  char result[HASH_LENGTH + SALT_LENGTH + 2];
+  snprintf(result, sizeof(result), "%s$%s", hash, salt); // Formato: hash$salt
 #endif
 
   wrenEnsureSlots(vm, 2);
@@ -41,10 +71,10 @@ void hashPassword(WrenVM* vm) {
 }
 
 void verifyPassword(WrenVM* vm) {
-#ifdef OPENSSL_OK
   int         result = 0;
   const char* password = wrenGetSlotString(vm, 1);
   const char* hash_and_salt = wrenGetSlotString(vm, 2);
+#ifdef OPENSSL_OK
 
   char stored_hash[65], stored_salt[33];
   strncpy(stored_hash, hash_and_salt, 64);
@@ -75,7 +105,18 @@ void verifyPassword(WrenVM* vm) {
   new_hash_str[64] = 0;
   result = strcmp(new_hash_str, stored_hash) == 0;
 #else
-  int result = 0;
+  char storedHash[HASH_LENGTH + 1];
+  char storedSalt[SALT_LENGTH + 1];
+  if(sscanf(hash_and_salt, "%64[^$]$%16s", storedHash, storedSalt) != 2) {
+    wrenEnsureSlots(vm, 2);
+    wrenSetSlotBool(vm, 0, 0);
+    return;
+  }
+  char saltedPassword[strlen(password) + SALT_LENGTH + 1];
+  snprintf(saltedPassword, sizeof(saltedPassword), "%s%s", password, storedSalt);
+  char computedHash[HASH_LENGTH + 1];
+  unsafe_hash(saltedPassword, computedHash);
+  result = (strncmp(computedHash, storedHash, HASH_LENGTH) == 0);
 #endif
   wrenEnsureSlots(vm, 2);
   wrenSetSlotBool(vm, 0, result);
