3e5d4a32340a739c4703d1b993a3395c877da454
Rodrigo Arce <albo@pragmore.com>
2026-07-23 13:59:28 -0300

    Remove unused script

--- tools/build.sh
+++
@@ -1,8 +0,0 @@
-# Build Linux app
-make clean && make
-cp build/bialet /tmp/
-# Build Windows app
-make clean && CC=x86_64-w64-mingw32-gcc make
-cp /usr/x86_64-w64-mingw32/bin/*.dll build/
-# Restore original
-cp /tmp/bialet build
